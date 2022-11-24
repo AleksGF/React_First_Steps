b867c5bb86c7790f5ba453dee6984a00bb2de4f4
AleksGF <aleksgf@mail.zp.ua>
2022-11-24 17:50:08 +0200

    Added userStatus API methods

--- src/api/profileAPI.js
+++ src/api/profileAPI.js
@@ -3,5 +3,13 @@ import axiosInstance from "./axiosInstance";
 export const profileAPI = {
   getUser (userId) {
     return axiosInstance.get(`profile/${userId}`).then(resp => resp.data);
+  },
+
+  getUserStatus (userId) {
+    return axiosInstance.get(`/profile/status/${userId}`);
+  },
+
+  setStatus (status) {
+    return axiosInstance.put(`/profile/status`, {status});
   }
 };
